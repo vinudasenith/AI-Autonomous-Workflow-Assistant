d4a8f896e473e0759fa40998fa0003498ed18bf1
vinudasenith <ha.vinudas@gmail.com>
2026-02-06 07:36:30 +0530

    feat: update planning_task

--- tasks/planning_tasks.py
+++ tasks/planning_tasks.py
@@ -1,7 +1,9 @@
 from crewai import Task
+from agents.planner_agent import planner_agent
 
 def planning_task(agent,user_input):
     return Task(
         description=f"Break this into steps: {user_input}",
-        agent=agent
+        agent=agent,
+        expected_output="A list of clear, actionable steps.",
     )
