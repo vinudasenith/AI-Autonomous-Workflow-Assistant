2649bd4f1ffc8b8c5976a0d20b33caba1376cf3a
vinudasenith <ha.vinudas@gmail.com>
2026-02-06 07:54:29 +0530

    feat: extend intent classifier CLI to run tasks

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from nlp.intent_classifier import classify_intent
+from crew import run_crew
+from tasks.planning_tasks import planning_task
 
-task = input("Enter a task: ")
-intent= classify_intent(task)
-
+user_task = input("Enter a task: ")
+intent= classify_intent(user_task)
 print("Detected intent:", intent)
+
+crew_output = run_crew(user_task)
+
+print("\nCrew Output:")
+print(crew_output)
