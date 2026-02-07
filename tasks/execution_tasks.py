87bc8606a572869564dd34d55ad52bd826901355
vinudasenith <ha.vinudas@gmail.com>
2026-02-07 08:35:26 +0530

    feat: integrate file, image, and web data sources

--- tasks/execution_tasks.py
+++ tasks/execution_tasks.py
@@ -1,9 +1,27 @@
 from crewai import Task
 from agents.executor_agent import executor_agent
+from tools.file_reader import read_pdf, read_word, read_csv
+from tools.image_processor import extract_text_from_image, basic_image_info
+from tools.web_scraper import fetch_web_text, fetch_api_data
 
-def execution_task(step, context):
+def execution_task(step, context,input_type="text",input_data=None):
+
+    if input_type=="pdf":
+        content=read_pdf(input_data)
+    elif input_type=="word":
+        content=read_word(input_data)
+    elif input_type=="csv":
+        content=read_csv(input_data)
+    elif input_type=="image":
+        content=extract_text_from_image(input_data)
+    elif input_type=="web":
+        content=fetch_web_text(input_data)
+    elif input_type=="api":
+        content=fetch_api_data(input_data)
+    else:
+        content=context
     return Task(
-        description=f"Summarize findings for: {step}\nContext: {context}",
+        description=f"Summarize findings for: {step}\nContext: {content}",
         agent=executor_agent,
         expected_output="A concise summary"
     )
